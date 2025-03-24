c971c5ff06ca68d15e02a6f46931f9d01858ed0e
OnSomeBread <mark1qaz@comcast.net>
2025-03-24 18:46:22 -0500

    added starting code for assign3

---
+++ assign3/proj3.cc
@@ -0,0 +1,7 @@
+#include "cs630.h"
+
+int RenderEngine::rd_bezier_curve(const string &vertex_type, int degree,
+                                  const vector<float> &vertex) {}
+
+int RenderEngine::rd_bezier_patch(const string &vertex_type, int u_degree,
+                                  int v_degree, const vector<float> &vertex) {}
