ea3fedecb238d8d2a5f1730421586ee47eafa393
Mark Schaefer <markschaefer@Marks-MacBook-Air.local>
2025-04-05 08:38:05 -0500

    made some optimizations and founds ways to generate more polys

--- assign3/proj3.cc
+++ assign3/proj3.cc
@@ -89,25 +89,13 @@ vector<V3> get_colors(const vector<float> &vertex) {
     return colors;
 }
 
-// simple factorial calculation that could be optimized out with faster
-// n_choose_k algo
-int factorial(int n) {
-    int result = 1;
-    for (int i = 2; i <= n; ++i) {
-        result *= i;
-    }
-    return result;
-}
-
-// the coefficient for the bernstein calculations
+// recursive n_choose_k
 float n_choose_k(int n, int k) {
-    return factorial(n) / (factorial(k) * factorial(n - k));
+    if (k == 0) return 1;
+    return (n * n_choose_k(n - 1, k - 1)) / k;
 }
-// float n_choose_k(int n, int k) {
-//     if (k == 0) return 1;
-//     return (n * n_choose_k(n - 1, k - 1)) / k;
-// }
 
+// the bernstein basis polynomials calculation
 float bernstein(int n, int k, float t) {
     return n_choose_k(n, k) * std::pow(1 - t, n - k) * std::pow(t, k);
 }
@@ -143,22 +131,21 @@ V3 eval_bezier_patch(vector<vector<V3>> &coords, float u, float v) {
 // function that both partials use to calculate the derivitive
 float deriv_bernstein(int n, int k, float t) {
     // left side = (1 - t)^(n - k)
-    float left_product_rule =
-        -(n - k) * std::pow(1 - t, n - k - 1) * std::pow(t, k);
+    float left = -(n - k) * std::pow(1 - t, n - k - 1) * std::pow(t, k);
     // right side = t^k
-    float right_product_rule = k * std::pow(t, k - 1) * std::pow(1 - t, n - k);
+    float right = k * std::pow(t, k - 1) * std::pow(1 - t, n - k);
 
     // YES ALL IF STATEMENTS HERE ARE NECESSARY
-    if (std::isnan(left_product_rule) && std::isnan(right_product_rule)) {
+    if (std::isnan(left) && std::isnan(right)) {
         return 0;
     }
-    if (std::isnan(left_product_rule)) {
-        return n_choose_k(n, k) * right_product_rule;
+    if (std::isnan(left)) {
+        return n_choose_k(n, k) * right;
     }
-    if (std::isnan(right_product_rule)) {
-        return n_choose_k(n, k) * left_product_rule;
+    if (std::isnan(right)) {
+        return n_choose_k(n, k) * left;
     }
-    return n_choose_k(n, k) * (left_product_rule + right_product_rule);
+    return n_choose_k(n, k) * (left + right);
 }
 
 // partial derivative with respect to u of bezier patch calculation
@@ -213,8 +200,7 @@ V3 avg_normal_vectors(vector<V3> &norms) {
     for (int i = 0; i < (int)norms.size(); ++i) {
         avg = avg + norms[i];
     }
-    avg = avg / -(int)norms.size();
-    return normalize(avg);
+    return normalize(avg / -(int)norms.size());
 }
 
 namespace render_direct {
@@ -363,15 +349,28 @@ int render_bezier_patch(const string &vertex_type, int u_degree, int v_degree,
     }
 
     // draw the bezier patch
-    for (int i = 0; i < (int)new_coords.size() - 1; ++i) {
-        for (int j = 0; j < (int)new_coords[i].size() - 1; ++j) {
+    for (int i = 0; i < (int)new_coords.size(); ++i) {
+        for (int j = 0; j < (int)new_coords[i].size(); ++j) {
             // a new way to handle more flags
-            vector<int> di = {0, 1, 0, 1};
-            vector<int> dj = {0, 0, 1, 1};
+            vector<int> di = {0};
+            vector<int> dj = {0};
+            if (j != new_coords[i].size() - 1) {
+                di.push_back(0);
+                dj.push_back(1);
+            }
+            if (i != new_coords.size() - 1 && j != new_coords[i].size() - 1) {
+                di.push_back(1);
+                dj.push_back(1);
+            }
+            if (i != new_coords.size() - 1) {
+                di.push_back(1);
+                dj.push_back(0);
+            }
+
             vector<V3> norms;
             vector<attr_point> attrs;
 
-            for (int k = 0; k < 4; ++k) {
+            for (int k = 0; k < (int)di.size(); ++k) {
                 attr_point a;
                 V3 p = new_coords[i + di[k]][j + dj[k]];
                 a.coord[0] = p.x;
@@ -405,11 +404,10 @@ int render_bezier_patch(const string &vertex_type, int u_degree, int v_degree,
                 poly_normal[2] = avg.z;
             }
 
-            // CC dir
-            poly_pipeline(attrs[0], MOVE);
-            poly_pipeline(attrs[2], MOVE);
-            poly_pipeline(attrs[3], MOVE);
-            poly_pipeline(attrs[1], DRAW);
+            for (int k = 0; k < attrs.size() - 1; ++k) {
+                poly_pipeline(attrs[k], MOVE);
+            }
+            poly_pipeline(attrs[attrs.size() - 1], DRAW);
         }
     }
 
