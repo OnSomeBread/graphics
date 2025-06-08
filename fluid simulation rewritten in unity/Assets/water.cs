053bf6a7b84f6693be55e5dd242e42224cc1f150
OnSomeBread <mark1qaz@comcast.net>
2025-06-08 10:17:40 -0500

    rewritten fluid simulation in unity

---
+++ fluid simulation rewritten in unity/Assets/water.cs
@@ -0,0 +1,188 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Unity.Mathematics;
+
+public class Water : MonoBehaviour{
+    // particle system settings
+    public float3 minBound = new float3(0);
+    public float3 maxBound = new float3(226);
+    public float particleSize = 1f;
+    public float gravity = 9.8f;
+    public float particleMass = 1f;
+    public float particleDamping = .05f;
+    public float densityRadius = 1.9f;
+    public float targetDensity = 2.75f;
+    public float pressureMultiplier = 50f;
+    public float viscosityMultiplier = .15f;
+    public float nearPressureMultiplier = 1f;
+    public Color particleColor = Color.blue;
+    public ComputeShader computeShader;
+    private Material material;
+
+    void createWaterCube(ref List<float4> particles, ref List<float4> velocities, float3 offset, float size, int rows, int cols, int planes){
+        float3 spacing = new float3(size / (float)rows, size / (float)cols, size / (float)planes);
+        float4 pos = new float4(0);
+
+        for (int i = 0; i < rows; ++i){
+            pos.x = offset.x + i * spacing.x;
+            for (int j = 0; j < cols; ++j){
+                pos.y = offset.y + j * spacing.y;
+                for (int k = 0; k < planes; ++k){
+                    pos.z = offset.z + k * spacing.z;
+
+                    float rx = UnityEngine.Random.Range(-spacing.x / 4, spacing.x / 4);
+                    float ry = UnityEngine.Random.Range(-spacing.y / 4, spacing.y / 4);
+                    float rz = UnityEngine.Random.Range(-spacing.z / 4, spacing.z / 4);
+
+                    particles.Add(pos + new float4(rx, ry, rz, 0));
+                    velocities.Add(new float4(0, 0, 0, 0));
+                }
+            }
+        }
+    }
+
+    private ComputeBuffer particleBuffer;
+    private ComputeBuffer velocitiesBuffer;
+    private ComputeBuffer predictedBuffer;
+    private ComputeBuffer nearbyBuffer;
+    private ComputeBuffer nearbyIdxBuffer;
+    private int CSPredicted;
+    private int CSNearbySort;
+    private int CSNearbyIdx;
+    private int CSCalcDensity;
+    private int CSCalcForces;
+    private int particleCount;
+    private Mesh quadMesh;
+    private Bounds boundingBox;
+    private int dispatchSize;
+    void Start(){
+        // TODO particle_count must be power of 2 for the parallel sort -- MUST FIX
+        const int N = 5;
+        int r = (int)Mathf.Pow(2f, N);
+        int c = (int)Mathf.Pow(2f, N);
+        int p = (int)Mathf.Pow(2f, N);
+        //float size = length(max_bound - min_bound) / 2. - 10.;
+        float size = maxBound.x / 2 - 10;
+
+        List<float4> particles = new List<float4>();
+        List<float4> velocities = new List<float4>();
+        createWaterCube(ref particles, ref velocities, minBound, maxBound.x, r, c, p);
+        //createWaterCube(ref particles, minBound + new float3(size + 15, 5, size + 15), size, r, c, p);
+        particleCount = particles.Count;
+        Debug.Log(particleCount);
+
+        material = new Material(Shader.Find("Unlit/waterShader"));
+        material.SetBuffer("_Particles", particleBuffer);
+        quadMesh = Resources.GetBuiltinResource<Mesh>("Quad.fbx");
+
+        // create all of the buffers
+        particleBuffer = new ComputeBuffer(particleCount, sizeof(float) * 4);
+        particleBuffer.SetData(particles);
+        velocitiesBuffer = new ComputeBuffer(particleCount, sizeof(float) * 4);
+        velocitiesBuffer.SetData(velocities);
+        predictedBuffer = new ComputeBuffer(particleCount, sizeof(float) * 4);
+        nearbyBuffer = new ComputeBuffer(particleCount, sizeof(int) * 2);
+        nearbyIdxBuffer = new ComputeBuffer(particleCount, sizeof(int));
+
+        CSPredicted = computeShader.FindKernel("CSPredicted");
+        computeShader.SetBuffer(CSPredicted, "_Particles", particleBuffer);
+        computeShader.SetBuffer(CSPredicted, "_Velocities", velocitiesBuffer);
+        computeShader.SetBuffer(CSPredicted, "_PredictedParticles", predictedBuffer);
+        computeShader.SetBuffer(CSPredicted, "_Nearby", nearbyBuffer);
+        computeShader.SetBuffer(CSPredicted, "_NearbyIdx", nearbyIdxBuffer);
+
+        CSNearbySort = computeShader.FindKernel("CSNearbySort");
+        computeShader.SetBuffer(CSNearbySort, "_Nearby", nearbyBuffer);
+
+        CSNearbyIdx = computeShader.FindKernel("CSNearbyIdx");
+        computeShader.SetBuffer(CSNearbyIdx, "_Nearby", nearbyBuffer);
+        computeShader.SetBuffer(CSNearbyIdx, "_NearbyIdx", nearbyIdxBuffer);
+
+        CSCalcDensity = computeShader.FindKernel("CSCalcDensity");
+        computeShader.SetBuffer(CSCalcDensity, "_PredictedParticles", predictedBuffer);
+        computeShader.SetBuffer(CSCalcDensity, "_Nearby", nearbyBuffer);
+        computeShader.SetBuffer(CSCalcDensity, "_NearbyIdx", nearbyIdxBuffer);
+
+        CSCalcForces = computeShader.FindKernel("CSCalcForces");
+        computeShader.SetBuffer(CSCalcForces, "_Particles", particleBuffer);
+        computeShader.SetBuffer(CSCalcForces, "_Velocities", velocitiesBuffer);
+        computeShader.SetBuffer(CSCalcForces, "_PredictedParticles", predictedBuffer);
+        computeShader.SetBuffer(CSCalcForces, "_Nearby", nearbyBuffer);
+        computeShader.SetBuffer(CSCalcForces, "_NearbyIdx", nearbyIdxBuffer);
+
+        computeShader.SetInt("_ParticleCount", particleCount);
+        boundingBox = new Bounds((minBound + maxBound) / 2f, maxBound - minBound);
+        dispatchSize = Mathf.CeilToInt(particleCount / 32f);
+
+        //Application.targetFrameRate = 60;
+    }
+
+    void Update(){
+        UpdateSettings();
+        
+        computeShader.Dispatch(CSPredicted, dispatchSize, 1, 1);
+
+        for (int k = 2; k <= particleCount; k *= 2){
+            for (int j = k / 2; j > 0; j /= 2){
+                computeShader.SetInt("_K", k);
+                computeShader.SetInt("_J", j);
+
+                computeShader.Dispatch(CSNearbySort, dispatchSize, 1, 1);
+            }
+        }
+
+        computeShader.Dispatch(CSNearbyIdx, dispatchSize, 1, 1);
+        computeShader.Dispatch(CSCalcDensity, dispatchSize, 1, 1);
+        // float4[] data = new float4[particleCount];
+        // predictedBuffer.GetData(data);
+        // for (int i = 0; i < particleCount; ++i){
+        //     Debug.Log(data[i]);
+        // }
+        // Debug.Log("END");
+
+        computeShader.Dispatch(CSCalcForces, dispatchSize, 1, 1);
+
+        material.SetBuffer("_Particles", particleBuffer);
+        Graphics.DrawMeshInstancedProcedural(quadMesh, 0, material, boundingBox, particleCount);
+    }
+
+    void UpdateSettings(){
+        computeShader.SetFloat("_ParticleMass", particleMass);
+        computeShader.SetFloat("_DensityRadius", densityRadius);
+        computeShader.SetFloat("_TargetDensity", targetDensity);
+        computeShader.SetFloat("_ParticleDamping", particleDamping);
+        computeShader.SetFloat("_PressureMultiplier", pressureMultiplier);
+        computeShader.SetFloat("_ViscosityMultiplier", viscosityMultiplier);
+        computeShader.SetFloat("_NearPressureMultiplier", nearPressureMultiplier);
+        computeShader.SetVector("_MinBound", new float4(minBound, 0));
+        computeShader.SetVector("_MaxBound", new float4(maxBound, 0));
+        computeShader.SetFloat("_Gravity", gravity);
+        computeShader.SetFloat("_DT", Mathf.Min(Time.deltaTime, .05f));
+
+        material.SetVector("_CameraRight", Camera.main.transform.right);
+        material.SetVector("_CameraUp", Camera.main.transform.up);
+
+        material.SetFloat("_Size", particleSize);
+        material.SetColor("_Color", particleColor);
+        
+        boundingBox = new Bounds((minBound + maxBound) / 2f, maxBound - minBound);
+    }
+
+    void OnRenderObject(){}
+
+    // draw the bounding box of the simulation
+    void OnDrawGizmos(){
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(boundingBox.center, boundingBox.size);
+    }
+
+    void OnDestroy(){
+        particleBuffer?.Release();
+        velocitiesBuffer?.Release();
+        predictedBuffer?.Release();
+        nearbyBuffer?.Release();
+        nearbyIdxBuffer?.Release();
+    }
+}
---
+++ fluid simulation rewritten in unity/Assets/waterShader.shader
@@ -0,0 +1,55 @@
+Shader "Unlit/waterShader"{
+    Properties{
+        _MainTex ("Particle Texture", 2D) = "white" {}
+    }
+    SubShader{
+        Tags { "RenderType"="Opaque" }
+        
+        Pass{
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            //#pragma multi_compile_instancing
+
+            #include "UnityCG.cginc"
+
+            StructuredBuffer<float4> _Particles;
+            float4 _Color;
+            float _Size;
+            float3 _CameraRight;
+            float3 _CameraUp;
+
+            struct appdata{
+                float3 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+                uint instanceID : SV_InstanceID;
+            };
+
+            struct v2f{
+                float4 vertex : SV_POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            v2f vert (appdata v){
+                v2f o;
+                float3 pos = _Particles[v.instanceID];
+                float3 worldPos = pos + (v.vertex.x * _CameraRight + v.vertex.y * _CameraUp) * _Size;
+
+                o.vertex = UnityWorldToClipPos(float4(worldPos, 1));
+                o.uv = v.uv;
+                return o;
+            }
+
+            sampler2D _MainTex;
+
+            fixed4 frag(v2f i) : SV_Target{
+                float2 quadCenter = i.uv * 2. - 1.;
+                float sqrDst = dot(quadCenter, quadCenter);
+                if(sqrDst > 1) discard;
+
+                return tex2D(_MainTex, i.uv) * float4(pow(_Color.rgb, 1.0/2.2), _Color.a);
+            }
+            ENDCG
+        }
+    }
+}
