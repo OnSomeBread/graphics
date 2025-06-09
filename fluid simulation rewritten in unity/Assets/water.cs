3f1092d21738a0b4f0a03340378844febdb1f566
OnSomeBread <mark1qaz@comcast.net>
2025-06-09 13:14:12 -0500

    several changes to improve performance and looks of simuation

--- fluid simulation rewritten in unity/Assets/water.cs
+++ fluid simulation rewritten in unity/Assets/water.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using Unity.Mathematics;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class Water : MonoBehaviour{
     // particle system settings
     public float3 minBound = new float3(0);
     public float3 maxBound = new float3(226);
-    public float particleSize = 1f;
     public float gravity = 9.8f;
     public float particleMass = 1f;
     public float particleDamping = .05f;
@@ -16,15 +16,14 @@ public class Water : MonoBehaviour{
     public float targetDensity = 2.75f;
     public float pressureMultiplier = 50f;
     public float viscosityMultiplier = .15f;
-    public float nearPressureMultiplier = 1f;
-    public Color particleColor = Color.blue;
+    public float nearPressureMultiplier = .01f;
     public ComputeShader computeShader;
-    private Material material;
+    public Material material;
 
     // creates a 3d cube of particles with even spacing between particles
-    void createWaterCube(ref List<float4> particles, ref List<float4> velocities, float3 offset, float size, int rows, int cols, int planes){
+    void createWaterCube(ref List<float3> particles, ref List<float3> velocities, float3 offset, float size, int rows, int cols, int planes){
         float3 spacing = new float3(size / rows, size / cols, size / planes);
-        float4 pos = new float4(0);
+        float3 pos = new float3(0);
 
         for (int i = 0; i < rows; ++i){
             pos.x = offset.x + i * spacing.x;
@@ -37,8 +36,8 @@ public class Water : MonoBehaviour{
                     float ry = UnityEngine.Random.Range(-spacing.y / 4, spacing.y / 4);
                     float rz = UnityEngine.Random.Range(-spacing.z / 4, spacing.z / 4);
 
-                    particles.Add(pos + new float4(rx, ry, rz, 0));
-                    velocities.Add(new float4(0, 0, 0, 0));
+                    particles.Add(pos + new float3(rx, ry, rz));
+                    velocities.Add(new float3(0, 0, 0));
                 }
             }
         }
@@ -69,23 +68,23 @@ public class Water : MonoBehaviour{
         //float size = length(max_bound - min_bound) / 2. - 10.;
         float size = maxBound.x / 2 - 10;
 
-        List<float4> particles = new List<float4>();
-        List<float4> velocities = new List<float4>();
+        List<float3> particles = new List<float3>();
+        List<float3> velocities = new List<float3>();
         createWaterCube(ref particles, ref velocities, minBound, maxBound.x, r, c, p);
         //createWaterCube(ref particles, minBound + new float3(size + 15, 5, size + 15), size, r, c, p);
         particleCount = particles.Count;
         Debug.Log(particleCount);
 
-        material = new Material(Shader.Find("Unlit/waterShader"));
+        //material = new Material(Shader.Find("Unlit/waterShader"));
         material.SetBuffer("_Particles", particleBuffer);
         quadMesh = Resources.GetBuiltinResource<Mesh>("Quad.fbx");
 
         // create all of the buffers
-        particleBuffer = new ComputeBuffer(particleCount, sizeof(float) * 4);
+        particleBuffer = new ComputeBuffer(particleCount, sizeof(float) * 3);
         particleBuffer.SetData(particles);
-        velocitiesBuffer = new ComputeBuffer(particleCount, sizeof(float) * 4);
+        velocitiesBuffer = new ComputeBuffer(particleCount, sizeof(float) * 3);
         velocitiesBuffer.SetData(velocities);
-        predictedBuffer = new ComputeBuffer(particleCount, sizeof(float) * 4);
+        predictedBuffer = new ComputeBuffer(particleCount, sizeof(float) * 3);
         densitiesBuffer = new ComputeBuffer(particleCount, sizeof(float) * 2);
         nearbyBuffer = new ComputeBuffer(particleCount, sizeof(int) * 2);
         nearbyIdxBuffer = new ComputeBuffer(particleCount, sizeof(int));
@@ -171,10 +170,6 @@ public class Water : MonoBehaviour{
         // settings for making the quads front facing
         material.SetVector("_CameraRight", Camera.main.transform.right);
         material.SetVector("_CameraUp", Camera.main.transform.up);
-
-        // quad settings
-        material.SetFloat("_Size", particleSize);
-        material.SetColor("_Color", particleColor);
         
         boundingBox = new Bounds((minBound + maxBound) / 2f, maxBound - minBound);
     }
@@ -191,6 +186,7 @@ public class Water : MonoBehaviour{
         particleBuffer?.Release();
         velocitiesBuffer?.Release();
         predictedBuffer?.Release();
+        densitiesBuffer?.Release();
         nearbyBuffer?.Release();
         nearbyIdxBuffer?.Release();
     }
--- fluid simulation rewritten in unity/Assets/waterShader.shader
+++ fluid simulation rewritten in unity/Assets/waterShader.shader
@@ -1,6 +1,11 @@
 Shader "Unlit/waterShader"{
     Properties{
         _MainTex ("Particle Texture", 2D) = "white" {}
+        _Size ("Particle Size", float) = 1
+        _Color ("Particle Color", Color) = (0, 0, .5, 1)
+        _Ambient ("Ambient Light", Color) = (.7, .8, .9, 1)
+        _LightDir ("Light Direction", Vector) = (-0.5, 0.5, -0.5, 1)
+        _Gloss("Gloss", Range(0,1)) = 1
     }
     SubShader{
         Tags { "RenderType"="Opaque" }
@@ -12,12 +17,17 @@ Shader "Unlit/waterShader"{
             //#pragma multi_compile_instancing
 
             #include "UnityCG.cginc"
+            #include "Lighting.cginc"
+            #include "AutoLight.cginc"
 
-            StructuredBuffer<float4> _Particles;
+            StructuredBuffer<float3> _Particles;
             float4 _Color;
             float _Size;
             float3 _CameraRight;
             float3 _CameraUp;
+            float4 _Ambient;
+            float4 _LightDir;
+            float _Gloss;
 
             struct appdata{
                 float3 vertex : POSITION;
@@ -28,15 +38,18 @@ Shader "Unlit/waterShader"{
             struct v2f{
                 float4 vertex : SV_POSITION;
                 float2 uv : TEXCOORD0;
+                float3 wPos : TEXCOORD1;
+                LIGHTING_COORDS(3,4)
             };
 
             v2f vert (appdata v){
                 v2f o;
-                float3 pos = _Particles[v.instanceID];
-                float3 worldPos = pos + (v.vertex.x * _CameraRight + v.vertex.y * _CameraUp) * _Size;
+                float3 worldPos =  _Particles[v.instanceID] + (v.vertex.x * _CameraRight + v.vertex.y * _CameraUp) * _Size;
 
                 o.vertex = UnityWorldToClipPos(float4(worldPos, 1));
                 o.uv = v.uv;
+                o.wPos = worldPos;
+                TRANSFER_VERTEX_TO_FRAGMENT(o);
                 return o;
             }
 
@@ -51,7 +64,21 @@ Shader "Unlit/waterShader"{
                 float quadz = sqrt(1 - sqrDst);
                 float3 quadNormal = normalize(float3(quadCenter, quadz));
 
-                return tex2D(_MainTex, i.uv) * float4(pow(_Color.rgb, 1.0/2.2), _Color.a);
+                //float3 lightDir = normalize(UnityWorldSpaceLightDir(i.wPos));
+                float attenuation = LIGHT_ATTENUATION(i);
+                float3 lambert = saturate(dot(quadNormal, normalize(_LightDir.xyz)));
+                float3 diffuse = lambert * attenuation;
+
+                // only apply ambient in base pass when doing multiple lights
+                diffuse += _Ambient.rbg;
+
+                // float3 V = normalize(_WorldSpaceCameraPos - i.wPos);
+                // float3 halfVec = normalize(_LightDir.xyz + V);
+
+                // float3 specularLight = saturate(dot(halfVec, quadNormal)) * (lambert > 0);
+                // specularLight = pow(specularLight, exp2(_Gloss * 11) + 2) * _Gloss * attenuation;
+
+                return tex2D(_MainTex, i.uv) * float4(diffuse * _Color.rgb, _Color.a);
             }
             ENDCG
         }
